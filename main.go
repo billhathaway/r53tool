267cbe8f9433141777cb2404fa73038eb10c25fe
Bill Hathaway <bill.hathaway@gmail.com>
2015-01-30 22:52:39 -0800

    turn functions into methods to simplify common variables

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+// This tool is designed to be used by operations to add or remove IP addresses from AWS Route53 record sets
+
 import (
 	"encoding/xml"
 	"flag"
@@ -13,28 +15,38 @@ import (
 	"github.com/awslabs/aws-sdk-go/gen/route53"
 )
 
-var region = "us-east-1"
+const defaultRegion = "us-east-1"
+
+type cli struct {
+	r53     *route53.Route53
+	log     *log.Logger
+	verbose bool
+}
 
 // zoneIDByName takes a record name and returns the Route53 zone ID
 // TODO: handle paging
-func zoneIDByName(r53 *route53.Route53, name string) (string, error) {
-	resp, err := r53.ListHostedZones(&route53.ListHostedZonesRequest{})
+func (c *cli) zoneIDByName(name string) (string, error) {
+	resp, err := c.r53.ListHostedZones(&route53.ListHostedZonesRequest{})
 	if err != nil {
 		return "", err
 	}
-	c := strings.Split(name, ".")
-	if len(c) < 3 {
+	components := strings.Split(name, ".")
+	if len(components) < 3 {
 		return "", fmt.Errorf("name must have at least one period")
 	}
-	name = strings.Join(c[len(c)-3:], ".")
+	name = strings.Join(components[len(components)-3:], ".")
 	for _, zone := range resp.HostedZones {
 		if *zone.Name == name {
 			// zone.ID looks like /hostedzone/Z22CR2RGPPKRQB but we just want the last part
-			c := strings.Split(*zone.ID, "/")
-			if len(c) != 3 {
+			components := strings.Split(*zone.ID, "/")
+			if len(components) != 3 {
 				return "", fmt.Errorf("problem splitting id from %s\n", *zone.ID)
 			}
-			return c[len(c)-1], nil
+			zoneID := components[len(components)-1]
+			if c.verbose {
+				c.log.Printf("zoneName=%s zoneID=%s\n", name, zoneID)
+			}
+			return zoneID, nil
 		}
 	}
 	return "", fmt.Errorf("zone %s not found", name)
@@ -45,11 +57,11 @@ func printResourceRecordSet(rrs route53.ResourceRecordSet) {
 	enc := xml.NewEncoder(os.Stdout)
 	enc.Indent("", "  ")
 	enc.Encode(rrs)
-	fmt.Println()
+	log.Println()
 }
 
 // addToARecordResourceRecordSet adds one or more IP addresses to the Resource Record Set
-func addToARecordResourceRecordSet(r53 *route53.Route53, zoneID string, rrs route53.ResourceRecordSet, ips ...string) error {
+func (c *cli) addToARecordResourceRecordSet(zoneID string, rrs route53.ResourceRecordSet, ips ...string) error {
 	if len(ips) == 0 {
 		return fmt.Errorf("at least one IP needs to be passed")
 	}
@@ -60,24 +72,26 @@ func addToARecordResourceRecordSet(r53 *route53.Route53, zoneID string, rrs rout
 	change := route53.Change{Action: aws.String("UPSERT"), ResourceRecordSet: &rrs}
 	changeBatch := route53.ChangeBatch{Changes: []route53.Change{change}}
 	req.ChangeBatch = &changeBatch
-	resp, err := r53.ChangeResourceRecordSets(req)
+	resp, err := c.r53.ChangeResourceRecordSets(req)
 	if err != nil {
 		return err
 	}
-	log.Printf("ChangeResourceRecordSets response=%+v\n", resp)
+	if c.verbose {
+		c.log.Printf("ChangeResourceRecordSets response=%+v\n", resp)
+	}
 	return nil
 }
 
 // getResourceRecordSet finds an existing resource record set matching the criteria
-func getResourceRecordSet(r53 *route53.Route53, zoneID string, recordName string, recordType string, setID string) (route53.ResourceRecordSet, error) {
-	zoneID, err := zoneIDByName(r53, recordName)
+func (c *cli) getResourceRecordSet(zoneID string, recordName string, recordType string, setID string) (route53.ResourceRecordSet, error) {
+	zoneID, err := c.zoneIDByName(recordName)
 	if err != nil {
 		return route53.ResourceRecordSet{}, err
 	}
 	req := route53.ListResourceRecordSetsRequest{HostedZoneID: &zoneID}
 	req.StartRecordName = aws.String(recordName)
 	req.StartRecordType = aws.String(recordType)
-	resp, err := r53.ListResourceRecordSets(&req)
+	resp, err := c.r53.ListResourceRecordSets(&req)
 	if err != nil {
 		return route53.ResourceRecordSet{}, err
 	}
@@ -115,7 +129,7 @@ func main() {
 	recordName := flag.String("name", "", "record name")
 	recordType := flag.String("type", "A", "record type")
 	setID := flag.String("setid", "", "record set identifier")
-	region := flag.String("region", "us-east-1", "AWS region")
+	region := flag.String("region", defaultRegion, "AWS region")
 	verbose := flag.Bool("v", false, "verbose")
 	flag.Parse()
 
@@ -123,38 +137,43 @@ func main() {
 	if len(ips) == 0 {
 		usage()
 	}
-
+	c := &cli{
+		log:     log.New(os.Stderr, "", log.LstdFlags),
+		verbose: *verbose,
+	}
 	auth, err := aws.EnvCreds()
+
 	if err != nil {
-		log.Fatal(err)
+		c.log.Fatal(err)
+
 	}
 
-	r53 := route53.New(auth, *region, http.DefaultClient)
+	c.r53 = route53.New(auth, *region, http.DefaultClient)
 
 	if !strings.HasSuffix(*recordName, ".") {
 		*recordName += "."
 	}
 
-	zoneID, err := zoneIDByName(r53, *recordName)
+	zoneID, err := c.zoneIDByName(*recordName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if *verbose {
-		log.Printf("zoneID for %s is %s", *recordName, zoneID)
+	if c.verbose {
+		c.log.Printf("zoneID for %s is %s", *recordName, zoneID)
 	}
 
-	rrs, err := getResourceRecordSet(r53, zoneID, *recordName, *recordType, *setID)
+	rrs, err := c.getResourceRecordSet(zoneID, *recordName, *recordType, *setID)
 	if err != nil {
-		log.Fatal(err)
+		c.log.Fatal(err)
 	}
 
-	if *verbose {
+	if c.verbose {
 		printResourceRecordSet(rrs)
 	}
 
-	err = addToARecordResourceRecordSet(r53, zoneID, rrs, ips...)
+	err = c.addToARecordResourceRecordSet(zoneID, rrs, ips...)
 	if err != nil {
-		log.Fatal(err)
+		c.log.Fatal(err)
 	}
 }
